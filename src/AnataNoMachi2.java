816da5abe3a2b81414bf0bdd6a3e6305cef38184
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2014-02-13 21:55:20 +0900

    B-3-1-2

---
+++ src/AnataNoMachi2.java
@@ -0,0 +1,53 @@
+public class AnataNoMachi2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		Denwa taro = new Denwa("太郎", "012-1234-5678");
+
+		for(int i=0; i<3; i++){
+			taro.denwaSuru();
+		}	
+		System.out.print("電話料金＜クリア前＞:");
+		System.out.println(taro.getRyokin());
+		
+		taro.clearRyokin();
+
+		System.out.print("電話料金＜クリア後＞:");
+		System.out.println(taro.getRyokin());
+		
+		System.out.print("電話番号＜変更前＞：");
+		System.out.println(taro.getBangou());
+		
+		taro.setBangou("111-1111-1111");
+		
+		System.out.print("電話番号＜変更後＞：");
+		System.out.println(taro.getBangou());
+		
+		System.out.print("会社名：");
+		System.out.println(taro.KAISYA);
+
+		System.out.print("型番号：");
+		System.out.println(taro.getKataban());
+
+		System.out.print("契約者：");
+		System.out.println(taro.getKeiyaku());
+
+	}
+	
+	static void printField(Denwa instance){
+		System.out.print(instance.KAISYA);
+		System.out.print(" ");
+		System.out.print(instance.getKataban());
+		System.out.print(" ");
+		System.out.print(instance.getKeiyaku());
+		System.out.print(" ");
+		System.out.print(instance.getBangou());
+		System.out.print(" ");
+		System.out.print(instance.getRyokin());
+		System.out.println();
+	}
+
+}
